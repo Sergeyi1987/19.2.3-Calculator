cb5364cee59820c92e92d83dd101c45ec688a743
1 <sergio_dx@list.ru>
2022-07-27 09:25:17 +0300

    add files

---
+++ tests/pytest.1.py
@@ -0,0 +1,31 @@
+import pytest
+from app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 3) == 6
+
+    def test_multiply_calculation_failed(self):
+        assert self.calc.multiply(self, 2, 3) == 5
+
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 4, 2) == 2
+
+    def test_division_calculation_failed(self):
+        assert self.calc.division(self, 4, 2) == 3
+
+    def test_subtraction_calculation_correctly(self):
+        assert self.calc.subtraction(self, 6, 2) == 4
+
+    def test_subtraction_calculation_failed(self):
+        assert self.calc.subtraction(self, 6, 2) == 3
+
+    def test_adding_calculation_correctly(self):
+        assert self.calc.subtraction(self, 4, 3) == 7
+
+    def test_adding_calculation_failed(self):
+        assert self.calc.subtraction(self, 4, 3) == 6
